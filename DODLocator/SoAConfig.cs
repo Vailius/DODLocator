58d030620422a8c84b4976426f3ffc946de1e891
Vailius <vassgx500x@gmail.com>
2026-03-03 21:21:57 +0300

    ADD | SoAConfig

---
+++ DODLocator/SoAConfig.cs
@@ -0,0 +1,48 @@
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using DODLocator.Curves;
+using DODLocator.IdGen;
+using DODLocator.Interfaces;
+
+namespace DODLocator
+{
+    public class SoAConfig
+    {
+        public readonly IMemoryGrowCurve MemoryGrow;
+        public readonly IIdentifierGenerator IdGenerator;
+        public readonly IMemoryAllocator Allocator;
+        public readonly int StartCapacity;
+
+        public SoAConfig([NotNullWhen(true)]IMemoryGrowCurve curve, 
+            [NotNullWhen(true)]IIdentifierGenerator idgen, 
+            [NotNullWhen(true)]IMemoryAllocator alloc, 
+            int startCapacity = 64)
+        {
+            Debug.Assert(curve != null, $"Null reference of {nameof(curve)}");
+            Debug.Assert(idgen != null, $"Null reference of {nameof(idgen)}");
+            Debug.Assert(alloc != null, $"Null reference of {nameof(alloc)}");
+            Debug.Assert(startCapacity > 0, $"{nameof(startCapacity)} <= 0");
+            MemoryGrow = curve;
+            IdGenerator = idgen;
+            Allocator = alloc;
+            StartCapacity = startCapacity;
+        }
+
+        public SoAConfig([NotNullWhen(true)]IIdentifierGenerator idgen, [NotNullWhen(true)]IMemoryAllocator alloc, int startCapacity = 64)
+            : this(new MemoryGrowCurveX2(), idgen, alloc, startCapacity) {}
+        public SoAConfig([NotNullWhen(true)]IMemoryGrowCurve curve, [NotNullWhen(true)]IMemoryAllocator alloc, int startCapacity = 64)
+            : this(curve, new IdentifierIterator(), alloc, startCapacity) {}
+        public SoAConfig([NotNullWhen(true)]IMemoryGrowCurve curve, [NotNullWhen(true)]IIdentifierGenerator idgen, int startCapacity = 64)
+            : this(curve, idgen, new DefaultAllocator(), startCapacity) {}
+        
+        public SoAConfig([NotNullWhen(true)]IMemoryGrowCurve curve, int startCapacity = 64)
+            : this(curve, new IdentifierIterator(), startCapacity) {}
+        public SoAConfig([NotNullWhen(true)]IIdentifierGenerator idgen, int startCapacity = 64)
+            : this(idgen, new DefaultAllocator(), startCapacity) {}
+        public SoAConfig([NotNullWhen(true)]IMemoryAllocator alloc, int startCapacity = 64)
+            : this(new IdentifierIterator(), alloc, startCapacity) {}
+        
+        public SoAConfig(int startCapacity = 64)
+            : this (new DefaultAllocator(), startCapacity) {}
+    }
+}
