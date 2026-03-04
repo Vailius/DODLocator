4f5217148781ffcc71533f52f1cc7efba2f343dc
Vailius <vassgx500x@gmail.com>
2026-03-04 20:32:02 +0300

    OPT | Visual output console style

--- DODLocator.Tests/Program.cs
+++ DODLocator.Tests/Program.cs
@@ -14,33 +14,34 @@ namespace DODLocator.Tests
         public static void Main(string[] args)
         {
             TEST("Mocks", TestMocks);
-            TEST("StructArray:MemoryLeaks", StructAttayTests.TestMemoryLeaks);
+            TEST("StructArray:MemoryLeaks", StructArrayTests.TestMemoryLeaks);
+            TEST("StructArray:MemoryTest", StructArrayTests.TestData);
         }
 
         private static void WriteColored(string prew, string colored, string after, ConsoleColor clr)
         {
-            Console.WriteLine(prew);
+            Console.Write(prew);
             Console.ForegroundColor = clr;
             Console.Write(colored);
             Console.ResetColor();
             Console.Write(after);
+            Console.Write('\n');
         }
 
         private static void TEST(string name, Action test)
         {
-            Console.WriteLine($"\nTest {name}");
             Stopwatch sw = Stopwatch.StartNew();
             try
             { test(); }
             catch (Exception e)
             {
                 sw.Stop();
-                WriteColored($"Test {name} is ", "FAILURE ", $"[{TimeSpan.FromTicks(sw.ElapsedTicks)}]", ConsoleColor.Red);
+                WriteColored($"\nTest {name} is ", "FAILURE ", $"[{TimeSpan.FromTicks(sw.ElapsedTicks)}]", ConsoleColor.Red);
                 Console.WriteLine($"\t\t ERROR : {e.Message}");
                 throw;
             }
             sw.Stop();
-            WriteColored($"Test {name} is ", "OKAY ", $"[{TimeSpan.FromTicks(sw.ElapsedTicks)}]", ConsoleColor.Green);
+            WriteColored($"\nTest {name} is ", "OKAY ", $"[{TimeSpan.FromTicks(sw.ElapsedTicks)}]", ConsoleColor.Green);
         }
     }
 }
